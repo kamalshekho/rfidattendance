ad30ac90b7e1dc77ea645344d17c810ead39a7c9
kamal shekho <k.shekho@yahoo.com>
2025-10-13 14:25:28 +0200

    update install.xml

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 require('../../config.php');
 
-$id = required_param('id', PARAM_INT); // Course module ID.
+$id = required_param('id', PARAM_INT);
 redirect(new moodle_url('/mod/rfidattendance/view.php', ['id' => $id]));
--- view.php
+++ view.php
@@ -3,14 +3,14 @@ require('../../config.php');
 require_login();
 
 $id = required_param('id', PARAM_INT); // Course module ID.
-$cm = get_coursemodule_from_id('simplebutton', $id, 0, false, MUST_EXIST);
+$cm = get_coursemodule_from_id('rfidattendance', $id, 0, false, MUST_EXIST);
 $course = $DB->get_record('course', ['id' => $cm->course], '*', MUST_EXIST);
 $context = context_module::instance($cm->id);
 
-require_capability('mod/simplebutton:view', $context);
+require_capability('mod/rfidattendance:view', $context);
 
-$PAGE->set_url('/mod/simplebutton/view.php', ['id' => $id]);
-$PAGE->set_title(get_string('pluginname', 'simplebutton'));
+$PAGE->set_url('/mod/rfidattendance/view.php', ['id' => $id]);
+$PAGE->set_title(get_string('pluginname', 'rfidattendance'));
 $PAGE->set_heading($course->fullname);
 
 echo $OUTPUT->header();
@@ -18,13 +18,13 @@ echo $OUTPUT->header();
 
 
 echo html_writer::tag('button',
-  get_string('pressme', 'simplebutton'),
-  ['id' => 'simplebutton', 'class' => 'btn btn-primary']
+  get_string('pressme', 'rfidattendance'),
+  ['id' => 'rfidattendance', 'class' => 'btn btn-primary']
 );
 
 $js = <<<JS
   document.addEventListener("DOMContentLoaded", function() {
-    const btn = document.getElementById("simplebutton");
+    const btn = document.getElementById("rfidattendance");
     btn.addEventListener("click", function() {
       const original = btn.textContent;
       btn.textContent = "Success!";
