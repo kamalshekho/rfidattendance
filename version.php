6715e141abe4e126f7a4c1715e97c8f055c635a7
kamal shekho <k.shekho@yahoo.com>
2025-10-14 00:41:53 +0200

    new version

--- version.php
+++ version.php
@@ -2,5 +2,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_rfidattendance'; // Full name of the plugin.
-$plugin->version  = 2025101001;     // YYYYMMDDXX...
+$plugin->version  = 2025101002;     // YYYYMMDDXX...
 $plugin->requires = 2022041900;     // Requires Moodle 4.0 or later (adjust if needed).
