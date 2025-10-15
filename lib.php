040ab42ceeb01e265a7cbe5631dc198abc3b9fbe
kamal shekho <k.shekho@yahoo.com>
2025-10-15 10:40:57 +0200

    update lib

--- lib.php
+++ lib.php
@@ -1,9 +1,6 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-/**
- * Returns information about the module.
- */
 function rfidattendance_supports($feature) {
   switch($feature) {
     case FEATURE_MOD_INTRO: return true;
@@ -11,18 +8,12 @@ function rfidattendance_supports($feature) {
   }
 }
 
-/**
- * Add a new rfidattendance instance.
- */
 function rfidattendance_add_instance($data, $mform = null) {
   global $DB;
   $data->timecreated = time();
   return $DB->insert_record('rfidattendance', $data);
 }
 
-/**
- * Update a rfidattendance instance.
- */
 function rfidattendance_update_instance($data, $mform = null) {
   global $DB;
   $data->id = $data->instance;
@@ -30,9 +21,6 @@ function rfidattendance_update_instance($data, $mform = null) {
   return $DB->update_record('rfidattendance', $data);
 }
 
-/**
- * Delete a rfidattendance instance.
- */
 function rfidattendance_delete_instance($id) {
   global $DB;
   return $DB->delete_records('rfidattendance', ['id' => $id]);
